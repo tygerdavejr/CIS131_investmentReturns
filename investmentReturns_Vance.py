759b52d724512dc456341fea9925a7d671bb6991
David Vance <dvance4@mail.pima.edu>
2025-06-06 11:16:44 -0700

    Update investmentReturns_Vance.py
    Basic nit-piddling to clean up the code.

--- investmentReturns_Vance.py
+++ investmentReturns_Vance.py
@@ -3,19 +3,21 @@
     action: a. Calculates interest on $1000 for 10, 20 and 30 years at 7%
             b. Display results
     author: David Vance
-    date: 20 January 2025
+    date: 6 June 2025
 '''
 
 
 #global constants and variables
 '''
     You can change your values for the program here.
-    Adjust principle and rate based on loan parameters.
+    Adjust principle and rate based on loan parameters,
+    and change the years in the list TIMES to set when
+    you want to check for returns.
 '''
 
 PRINCIPLE = 1000
 RATE = .07
-
+TIMES = [10, 20, 30]
 
 def calculateInterestRates(time):
     '''
@@ -51,17 +53,17 @@ def main():
         to process the amountAccrued at 10, 20 and 30 years.
         Change the years in the list times to adjust your formula.
     '''
-    times = [10, 20, 30]
+
 
     # Process each year in the list one at a time until the list is complete
 
-    for time in times:
+    for time in TIMES:
     
         amountAccrued = calculateInterestRates(time)
 
         print('The following output displays the investment returns as years and amount accrued')
-        print(f'on a principle of ${PRINCIPLE} at a rate of {(RATE * 100):.0f}%.\n')
-        print(f'{time} years:  ${amountAccrued:.2f}')
+        print(f'on a principle of ${PRINCIPLE} at a rate of {(RATE * 100):.0f}%.')
+        print(f'{time} years:  ${amountAccrued:.2f}%.\n')
         
     return
 
